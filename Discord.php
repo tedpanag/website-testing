3c6db0e4423fa17b32148a3f25a64a1ebf45c3b9
tedpanag <dimpan008@gmail.com>
2023-10-10 15:39:14 +0300

    some message

---
+++ Discord.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+Discord:
+</head>
+<body>
+    <h1>Tors</h1>
+    <p>https://discord.gg/2cEJkM8MNB</p>
+        <link rel="stylesheet" href="https://discord.gg/2cEJkM8MNB">
+    <p></p>
+    <p></p>
+</body>
+</html>
---
+++ Reddit.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+Reddit:
+</head>
+<body>
+    <h1>tors_reddit</h1>
+    <p>https://reddit.com/r/tors_reddit/s/7Ybj84VQ5i</p>
+        <link rel="stylesheet" href="https://reddit.com/r/tors_reddit/s/7Ybj84VQ5i">
+    <p></p>
+    <p></p>
+</body>
+</html>
