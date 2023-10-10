6e6337d9aa290ba508e4ab9adab83f8017c0a888
tedpanag <dimpan008@gmail.com>
2023-10-10 15:30:15 +0300

    some message

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Instagram:
 <body>
     <h1>inta_tors</h1>
     <p>https://www.instagram.com/tors_insta/</p>
-        <link rel="import" href="https://www.instagram.com/tors_insta/.html">
+        <link rel="stylesheet" href="https://www.instagram.com/tors_insta/">
     <p></p>
     <p></p>
 </body>
